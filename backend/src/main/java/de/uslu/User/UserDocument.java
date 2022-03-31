a3d249d642b7aa93ce91f70cf135c4df5f74998f
Samet Uslu <samet_u@hotmail.de>
2022-03-31 16:46:01 +0200

    user login

---
+++ backend/src/main/java/de/uslu/User/UserDocument.java
@@ -0,0 +1,16 @@
+package de.uslu.User;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+@Data
+public class UserDocument {
+
+    @Id
+    private String id;
+    private String username;
+    private String password;
+
+}
